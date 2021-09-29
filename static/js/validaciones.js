ceb1340011350e31be9790581c0243e108dde483
Juan Marcos <jmarascutti@gmail.com>
2021-09-29 15:37:35 -0300

    Se agregan nuevas validaciones y se mejora aspectos visuales del template

--- static/js/validaciones.js
+++ static/js/validaciones.js
@@ -1,51 +1,101 @@
 
 function validarFormulario() {
 
-  // NOMBRE
-    if (document.formularioSociedad.nombre.value.length==0 ) {    
-      let mensaje = "Por favor, ingresa tu nombre."
-      document.formularioSociedad.nombre.focus();    
+  // NOMBRE DE LA SOCIEDAD
+    if (document.formularioSociedad.nombreSociedad.value.length==0 ) {    
+      let mensaje = "Por favor, ingresa el nombre de la sociedad."
+      document.formularioSociedad.nombreSociedad.focus();    
       mostrarModalMensaje(mensaje)
       return false; 
     } 
     
-    if (document.formularioSociedad.nombre.value.length > 50 ||
-        document.formularioSociedad.nombre.value.length <= 2) {
-        let mensaje = 'Por favor, tu nombre debe tener entre 3 y 50 caracteres.';
-        document.formularioSociedad.nombre.focus();
+    if (document.formularioSociedad.nombreSociedad.value.length > 50 ||
+        document.formularioSociedad.nombreSociedad.value.length <= 2) {
+        let mensaje = 'Por favor, el nombre de la sociedad debe tener entre 3 y 50 caracteres.';
+        document.formularioSociedad.nombreSociedad.focus();
         mostrarModalMensaje(mensaje);
         return false;
     }
 
-    if (/[^a-zA-ZÁÉÍÓÚáéíóúñÑ' ]+/.test(document.formularioSociedad.nombre.value)) {
-        let mensaje = 'Por favor, tu nombre debe contener solo letras.';
-        document.formularioSociedad.nombre.focus();
+    if (/[^a-zA-ZÁÉÍÓÚáéíóúñÑ' ]+/.test(document.formularioSociedad.nombreSociedad.value)) {
+        let mensaje = 'Por favor, el nombre de la sociedad debe contener solo letras.';
+        document.formularioSociedad.nombreSociedad.focus();
         mostrarModalMensaje(mensaje);
         return false;
     }
 
-    // SOCIO
-    if (document.formularioSociedad.socio.value.length==0 ) {    
+    // FECHA DE CREACION
+    if (document.formularioSociedad.fechaCreacion.value.length==0 ) {    
+      let mensaje = "Por favor, ingresa la fecha de creación."
+      document.formularioSociedad.fechaCreacion.focus();    
+      mostrarModalMensaje(mensaje)
+      return false;
+ 
+      // VALIDAR QUE LA FECHA DE CREACION SEA MENOR O IGUAL QUE LA FECHA ACTUAL - HACER
+
+    } /*else {
+      let fecha =  new Date();
+      mensaje = fecha.getDate();
+      mostrarModalMensaje(mensaje)
+      return false; 
+    } */
+
+    //
+   /* if () {
+      let mensaje = "Por favor, la fecha de creación debe ser menor o igual a la fecha actual."
+      document.formularioSociedad.fechaCreacion.focus();    
+      mostrarModalMensaje(mensaje)
+
+    } */
+
+    // APELLIDO DEL SOCIO
+    if (document.formularioSociedad.apellidoSocio.value.length==0 ) {    
+      let mensaje = "Por favor, ingresa el apellido del socio."
+      document.formularioSociedad.apellidoSocio.focus();    
+      mostrarModalMensaje(mensaje)
+      return false; 
+    } 
+    
+    if (document.formularioSociedad.apellidoSocio.value.length > 50 ||
+        document.formularioSociedad.apellidoSocio.value.length <= 2) {
+        let mensaje = 'Por favor, el apellido del socio debe tener entre 3 y 50 caracteres.';
+        document.formularioSociedad.apellidoSocio.focus();
+        mostrarModalMensaje(mensaje);
+        return false;
+    }
+
+    if (/[^a-zA-ZÁÉÍÓÚáéíóúñÑ' ]+/.test(document.formularioSociedad.apellidoSocio.value)) {
+        let mensaje = 'Por favor, el apellido del socio debe contener sólo letras.';
+        document.formularioSociedad.apellidoSocio.focus();
+        mostrarModalMensaje(mensaje);
+        return false;
+    }
+
+    // NOMBRE DEL SOCIO
+    if (document.formularioSociedad.nombreSocio.value.length==0 ) {    
       let mensaje = "Por favor, ingresa el nombre del socio."
-      document.formularioSociedad.socio.focus();    
+      document.formularioSociedad.nombreSocio.focus();    
       mostrarModalMensaje(mensaje)
       return false; 
     } 
     
-    if (document.formularioSociedad.socio.value.length > 50 ||
-        document.formularioSociedad.socio.value.length <= 2) {
+    if (document.formularioSociedad.nombreSocio.value.length > 50 ||
+        document.formularioSociedad.nombreSocio.value.length <= 2) {
         let mensaje = 'Por favor, el nombre del socio debe tener entre 3 y 50 caracteres.';
-        document.formularioSociedad.socio.focus();
+        document.formularioSociedad.nombreSocio.focus();
         mostrarModalMensaje(mensaje);
         return false;
     }
 
-    if (/[^a-zA-ZÁÉÍÓÚáéíóúñÑ' ]+/.test(document.formularioSociedad.socio.value)) {
+    if (/[^a-zA-ZÁÉÍÓÚáéíóúñÑ' ]+/.test(document.formularioSociedad.nombreSocio.value)) {
         let mensaje = 'Por favor, el nombre del socio debe contener sólo letras.';
-        document.formularioSociedad.socio.focus();
+        document.formularioSociedad.nombreSocio.focus();
         mostrarModalMensaje(mensaje);
         return false;
     }
+
+    // PORCENTAJE DE APORTES
+    // HACER
 
     // ESTATUTO DE CONFORMACION
     if (document.formularioSociedad.estatuto.value.length == 0)  {      
@@ -58,12 +108,20 @@ function validarFormulario() {
     var input = document.getElementById('estatuto');
 		var file = input.files[0];
     if (file.size > 3000000) {		
-			  let $mensaje = 'Por favor, el estatuto de conformacón debe pesar menos de 3 mb.';
+			  let $mensaje = 'Por favor, el estatuto de conformacón debe pesar menos de 3 megabytes.';
         mostrarModalMensaje($mensaje);
 	  		return false;
 		}
 
-    // FALTA VALIDAR TIPO DE ARCHIVO
+    var fileInput = document.getElementById('estatuto');
+    var filePath = fileInput.value;
+    var extensionesPermitidas = /(.pdf|.docx|.odt)$/i;
+    if (!extensionesPermitidas.exec(filePath)) {
+        fileInput.value = '';
+        let $mensaje = 'Por favor, el formato de archivo del estatuto de conformación debe ser .docx, .odt ó .pdf';
+        mostrarModalMensaje($mensaje);    
+        return false;
+    }
 
      // DOMICILIO LEGAL
      if (document.formularioSociedad.domicilioLegal.value.length==0 ) {    
@@ -118,18 +176,24 @@ function validarFormulario() {
         return false;
     }
 
+    // LOS PAISES Y ESTADOS SOLO SE DEBEN VALIDAR SI SE TILDÓ "EXPORTA A OTROS PAISES" - HACER
+
     // PAISES DE EXPORTACION
     if (document.formularioSociedad.paisDeExportacion.value == 0 || document.formularioSociedad.paisDeExportacion.value== "") {
-      let $mensaje = 'Por favor, seleccioná un país de exportación.';
+      let $mensaje = 'Por favor, seleccioná un país.';
       document.formularioSociedad.paisDeExportacion.focus();
       mostrarModalMensaje($mensaje)
       return false;
     }
+
+    // ESTADOS DE EXPORTACION
+    if (document.formularioSociedad.estadoDeExportacion.value == 0 || document.formularioSociedad.estadoDeExportacion.value== "") {
+      let $mensaje = 'Por favor, seleccioná un estado.';
+      document.formularioSociedad.estadoDeExportacion.focus();
+      mostrarModalMensaje($mensaje)
+      return false;
+    }
    
-
-  
-
-
 	return true;
 }
 
