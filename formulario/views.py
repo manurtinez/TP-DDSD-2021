311580196efce4b012eb712a3c209d3a9a898e0f
Manu Martinez <mantinez@gmail.com>
2021-10-07 21:57:51 -0300

    muevo metodo de bonita fuera de clase view

--- formulario/views.py
+++ formulario/views.py
@@ -3,13 +3,10 @@ from rest_framework import status, permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from requests import exceptions
-
 from formulario.models import Socio, SociedadAnonima
 from formulario.serializers import FileSerializer, SociedadAnonimaRetrieveSerializer, SociedadAnonimaSerializer, SocioSerializer
 
-from utils.bonita_service import bonita_api_call, bonita_login
-from utils.types import java_types
+from utils.bonita_service import start_bonita_process
 
 # # el objeto env se usa para traer las variables de entorno
 env = environ.Env()
@@ -80,7 +77,7 @@ class SociedadAnonimaViewSet(viewsets.ModelViewSet):
 
             # Se inicia el proceso en bonita si se esta local
             if env('DJANGO_DEVELOPMENT') == 'True':
-                if (not self.start_bonita_process(new_sa)):
+                if (not start_bonita_process(new_sa)):
                     print(
                         '---> Hubo algun problema al iniciar el caso de bonita. Sin embargo, la SA fue creada correctamente')
             return Response(data=response_data, status=status.HTTP_201_CREATED)
@@ -106,41 +103,3 @@ class SociedadAnonimaViewSet(viewsets.ModelViewSet):
     #         return SociedadAnonimaSerializer
     #     else:
     #         return SociedadAnonimaRetrieveSerializer
-
-    def start_bonita_process(self, new_sa):
-        """
-        Este metodo dispara una instancia del proceso en bonita.
-        NOTA: hay que refactorizar el login y implementar manejo de excepciones.
-        """
-        try:
-            # Por ahora, el login hardcodeado cada vez que se crea una SA
-            bonita_login()
-
-            # Esto devuelve un array de procesos, en nuestro caso, uno solo, como Dict
-            bonita_process = bonita_api_call('process', 'get', '?s=Proceso')[0]
-
-            # Se arranca la instancia (caso) del proceso
-            bonita_case = bonita_api_call('case', 'post', data={
-                "processDefinitionId": bonita_process['id']})
-
-            # Se setean las variables id y name al caso
-            bonita_api_call('caseVariable', 'put', f'/{bonita_case["id"]}/id', {
-                'type': java_types[type(new_sa.id).__name__], 'value': new_sa.id})
-
-            bonita_api_call('caseVariable', 'put', f'/{bonita_case["id"]}/name', {
-                'type': java_types[type(new_sa.name).__name__], 'value': new_sa.name})
-
-            # Esta parte no fue necesaria por ahora pero la dejo por si sirve para despues
-
-            # Se mandan las variables una por una al proceso
-            # for var, value in sa_vars:
-            #     # Se deben ignorar las variables que no nos interesan
-            #     # (hardcodeadas porque es mas facil y no van a cambiar)
-            #     if (var not in ['id', '_state', 'comformation_statute', 'export_countries']):
-            #         bonita_api_call('caseVariable', 'put', f'/{bonita_case["id"]}/{var}', {
-            #             'type': java_types[type(value).__name__], 'value': value})
-
-            return True
-        except exceptions.RequestException as e:
-            print(e)
-            return False
--- utils/bonita_service.py
+++ utils/bonita_service.py
@@ -5,6 +5,8 @@ from importlib import import_module
 
 from django.conf import settings
 
+from .types import java_types
+
 SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 # Creo el store de la sesion para guardar cookies y demas cosas
@@ -51,3 +53,42 @@ def bonita_api_call(resource,  method, url_params='', data={}):
         raise requests.exceptions.RequestException(
             f'El request para recurso {resource} de bonita fallo con codigo {response.status_code}')
     return response.json() if resource != 'caseVariable' else True
+
+
+def start_bonita_process(new_sa):
+    """
+    Este metodo dispara una instancia del proceso en bonita para una sociedad anonima dada.
+    NOTA: hay que refactorizar el login y implementar manejo de excepciones.
+    """
+    try:
+        # Por ahora, el login hardcodeado cada vez que se crea una SA
+        bonita_login()
+
+        # Esto devuelve un array de procesos, en nuestro caso, uno solo, como Dict
+        bonita_process = bonita_api_call('process', 'get', '?s=Proceso')[0]
+
+        # Se arranca la instancia (caso) del proceso
+        bonita_case = bonita_api_call('case', 'post', data={
+            "processDefinitionId": bonita_process['id']})
+
+        # Se setean las variables id y name al caso
+        bonita_api_call('caseVariable', 'put', f'/{bonita_case["id"]}/id', {
+            'type': java_types[type(new_sa.id).__name__], 'value': new_sa.id})
+
+        bonita_api_call('caseVariable', 'put', f'/{bonita_case["id"]}/name', {
+            'type': java_types[type(new_sa.name).__name__], 'value': new_sa.name})
+
+        # Esta parte no fue necesaria por ahora pero la dejo por si sirve para despues
+
+        # Se mandan las variables una por una al proceso
+        # for var, value in sa_vars:
+        #     # Se deben ignorar las variables que no nos interesan
+        #     # (hardcodeadas porque es mas facil y no van a cambiar)
+        #     if (var not in ['id', '_state', 'comformation_statute', 'export_countries']):
+        #         bonita_api_call('caseVariable', 'put', f'/{bonita_case["id"]}/{var}', {
+        #             'type': java_types[type(value).__name__], 'value': value})
+
+        return True
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return False
