ce7a4c2cb8f04aac2d801137852852fc125f453b
Juan Marcos <jmarascutti@gmail.com>
2021-10-16 21:10:58 -0300

    Se agregan nuevas vistas al proyecto

--- formulario/urls.py
+++ formulario/urls.py
@@ -17,5 +17,11 @@ urlpatterns = [
     path('sociedad_anonima/alta', TemplateView.as_view(
         template_name='altaDeFormulario.html'), name="alta_formulario"), 
     path('sociedad_anonima/ver', TemplateView.as_view(
-        template_name='verSociedadPublica.html'), name="ver_sociedad_publica")
+        template_name='verSociedadPublica.html'), name="ver_sociedad_publica"),
+    path('sociedad_anonima/listadoDeSociedadesPendientes', TemplateView.as_view(
+        template_name='listadoDeSociedadesPendientes.html'), name="listado_sociedades_pendientes_aprobacion"),
+    path('sociedad_anonima/listadoDeSociedadesEstampillar', TemplateView.as_view(
+        template_name='listadoDeSociedadesEstampillar.html'), name="listado_sociedades_estampillar"),
+    path('sociedad_anonima/verDetalle', TemplateView.as_view(
+        template_name='verDetalleSociedad.html'), name="detalle_sociedad_anonima"),
 ]
