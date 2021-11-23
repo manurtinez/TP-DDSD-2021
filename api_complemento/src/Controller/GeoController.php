2ff531eba03abefdf35bc8614f0ef9d468c06e56
Tubaro Federico <tubarofederico@gmail.com>
2021-11-22 22:14:16 -0300

    Se agregaron los endpoint para obtener continentes, paises y estados

--- api_complemento/src/Controller/GeoController.php
+++ api_complemento/src/Controller/GeoController.php
@@ -2,32 +2,107 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Estampillado;
-
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7;
 
 /**
- * @return JsonResponse
- * @Route("/complemento", name="api_geo")
+  * @return JsonResponse
+  * @Route("/api/geo")
  */
-class GeoController extends ApiController
+class GeoController extends AbstractController
 {
-  
-    /**
-     * @return JsonResponse
-     * @Route("/prueba", name="get_prueba", methods={"GET"})
-     */
-    public function getPrueba(){
+    private $client;
 
-        return $this->response([
-            "num_expediente" => "bom bn",
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'http://countries.trevorblades.com'
         ]);
     }
 
+    /**
+     * @Route("/continente", name="geo_continentes", methods={"GET"})
+     */
+    public function getContinentesAction(Request $request)
+    {     
+        $response = $this->client->request('POST', '', [
+            'json' => [
+                "operationName" => null,
+                "variables" => "{}",
+                "query" => "{
+                    continents {
+                        code
+                        name
+                    }
+                }"
+            ],
+        ]);
+
+        $datos = json_decode((string) $response->getBody());
+        return new JsonResponse($datos->data->continents, 200);
+    }
+
+    /**
+     * @Route("/continente/{code}/pais", name="geo_paises", methods={"GET"})
+     */
+    public function getPaisesAction($code, Request $request)
+    {   
+        if($code == null){
+            return new JsonResponse("Debe indicar el codigo del continente", 404 );
+        }
+
+        $response = $this->client->request('POST', '', [
+            'json' => [
+                "operationName" => null,
+                "variables" => '{"continentCode": "'.$code.'"}',
+                "query" => '
+                    query($continentCode: ID!){
+                        continent(code: $continentCode) {
+                            countries {name code }
+                        }
+                    }'
+            ],
+        ]);
+
+        $datos = json_decode((string) $response->getBody());
+        if($datos->data->continent == null){
+            return new JsonResponse("No se encontraron paises para el continente indicado", 404 );
+        }
+
+        return new JsonResponse($datos->data->continent->countries, 200);
+    }
+
+    /**
+     * @Route("/pais/{code}/estado", name="geo_estados", methods={"GET"})
+     */
+    public function getEstadosAction($code,Request $request)
+    {     
+        if($code == null){
+            return new JsonResponse("Debe indicar el codigo del Pais", 404 );
+        }
+
+        $response = $this->client->request('POST', '', [
+            'json' => [
+                "operationName" => null,
+                "variables" => '{"paisCode": "'.$code.'"}',
+                "query" => '
+                    query($paisCode: ID!){
+                        country(code: $paisCode) {
+                            states {name code }
+                        }
+                    }'
+            ],
+        ]);
+
+        $datos = json_decode((string) $response->getBody());
+        if($datos->data->country == null){
+            return new JsonResponse("No se encontraron estados para el pais indicado", 404 );
+        }
+
+        return new JsonResponse($datos->data->country->states, 200);
+    }
+    
 }
