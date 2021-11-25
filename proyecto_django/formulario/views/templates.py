d9a3de8690dec17bba0ba5a6753cdbd1b47e99e4
Manuel Martinez <mantinez@gmail.com>
2021-11-25 11:03:20 -0300

    agrego views de template con permisos

--- proyecto_django/formulario/views/templates.py
+++ proyecto_django/formulario/views/templates.py
@@ -1,6 +1,27 @@
 
+from django.shortcuts import render
 from formulario.permissions import template_guard
 
 
 def pendientes(request):
     return template_guard(request, 'listadoDeSociedadesPendientes.html', 'Empleado mesa')
+
+
+def a_evaluar_estatuto(request):
+    return template_guard(request, 'listadoDeSociedadesEvaluacionEstatuto.html', 'Escribano')
+
+
+def dashboard(request):
+    return template_guard(request, 'dashboard.html', 'Direccion')
+
+
+def login_template(request):
+    return render(request, 'login.html')
+
+
+def ver_sa_publica(request):
+    return render(request, 'verSociedadPublica.html')
+
+
+def alta_sa(request):
+    return render(request, 'altaDeFormulario.html')
